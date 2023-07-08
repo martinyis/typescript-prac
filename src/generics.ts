a2b5a38ca8bbdd482f061777a8e7b9ab99ddd517
martinyis <martinyis11@gmail.com>
2023-07-08 10:04:16 -0700

    Add som einfo to generics file

--- src/generics.ts
+++ src/generics.ts
@@ -15,3 +15,34 @@ class Channel<T> {
     return this.name;
   }
 }
+
+const entity2 = <T>(args: T): T => {
+  return args;
+};
+
+interface IPair<K, V> {
+  key: K;
+  value: V;
+}
+
+const pair1: IPair<number, string> = {
+  key: 1,
+  value: "Joe",
+};
+
+type TypeCity<R, V> = {
+  city: R;
+  country: V;
+};
+
+const city: TypeCity<string, string> = {
+  city: "London",
+  country: "UK",
+};
+
+type TypeLength = {
+  length: number;
+};
+function getNameLength<T extends TypeLength>(entity: T): number {
+  return entity.length;
+}
